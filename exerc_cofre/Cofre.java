668714c93323034cb8b4665285816349035b993a
gustevito <dev.gustavo.flores@gmail.com>
2025-08-11 09:28:49 -0300

    classe app

---
+++ exerc_cofre/Cofre.java
@@ -0,0 +1,16 @@
+public class Cofre {
+    Moeda[] moedas;
+    int qtdMoedas;
+    
+    public Cofre(){
+
+    }
+
+    public boolean insere(Moeda moeda){ return false; }
+    public Moeda retira(){ return 0; }
+    public int getQtdMoedas(){ return qtdMoedas; }
+    public in getQtdMoedasPorTipo(NomeMoeda nomeMoeda){}
+    public int getValorTotalCentavos(){ return 0; }
+    public double getValorTotalReais(){ return 0; }
+
+}
