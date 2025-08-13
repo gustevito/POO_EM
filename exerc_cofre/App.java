63fb6871bcf762ca719c1959eb8e44d32e29d08e
gustevito <dev.gustavo.flores@gmail.com>
2025-08-13 08:32:08 -0300

    tad

--- exerc_cofre/App.java
+++ exerc_cofre/App.java
@@ -2,9 +2,29 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.println("Digite a opção que desejas operar:\n");
-        System.out.println("1 - Inserir moedas");
-        System.out.println("2 - Conferir moedas");
-        System.out.println("3 - Retirar moedas");
+        
+        while (loop) {
+            System.out.println("Digite a opção que desejas operar:\n");
+            System.out.println("1 - Inserir moedas\n");
+            System.out.println("2 - Conferir moedas\n");
+            System.out.println("3 - Retirar moedas\n");
+            int n = in.nextInt();
+            in.nextLine();
+
+            switch (n) {
+                case 1:
+                    
+                    break;
+                case 2:
+
+                    break;
+                case 3:
+
+                    break;
+                default:
+                    System.out.println("ERRO: Insira um valor inteiro de 1 a 3.");
+                    break;
+            }
+        }
     }
 }
