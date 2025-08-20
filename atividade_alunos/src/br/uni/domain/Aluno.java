7bca7b64135f97bf7fcabaaa9059d9f640d5572e
gustevito <dev.gustavo.flores@gmail.com>
2025-08-20 09:13:12 -0300

    atividade (exp.)

--- atividade_alunos/src/br/uni/domain/Aluno.java
+++ atividade_alunos/src/br/uni/domain/Aluno.java
@@ -1,3 +1,34 @@
+import java.time.LocalDate;
+
 public class Aluno {
-    
+    private String nome;
+    private String matricula;
+    private LocalDate dataDeNascimento;
+
+
+    public String getNome() {
+        return nome;
+    }
+    public String getMatricula() {
+        return matricula;
+    }
+    public LocalDate getDataDeNascimento() {
+        return dataDeNascimento;
+    }
+
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+    public void setDataDeNascimento(LocalDate dataDeNascimento) {
+        this.dataDeNascimento = dataDeNascimento;
+    }
+
+    public int criaMatricula(){
+        
+    }
+
 }
